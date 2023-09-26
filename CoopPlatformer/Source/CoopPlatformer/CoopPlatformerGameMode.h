f0d324107594d6a41da47ebccb1a153b435c661a
Cael Rowley <drdeus@pm.me>
2023-09-26 19:00:25 +0200

    chore: setup .gitignore and .gitattributes for an unreal engine project

---
+++ CoopPlatformer/Source/CoopPlatformer/CoopPlatformerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoopPlatformerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACoopPlatformerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACoopPlatformerGameMode();
+};
+
+
+
