baa8fd20885299e79be2f06133d10b6e486cf07f
Cael Rowley <drdeus@pm.me>
2023-09-27 13:29:04 +0200

    feat: add MyBox which replicates on client from server

---
+++ CoopPlatformer/Source/CoopPlatformer/MyBox.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyBox.h"
+#include "Net/UnrealNetwork.h"
+
+// Sets default values
+AMyBox::AMyBox()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ReplicatedVar = 100.0f;
+	bReplicates = true;
+}
+
+// Called when the game starts or when spawned
+void AMyBox::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetReplicateMovement(true);
+
+	if (HasAuthority() || GetLocalRole() == ROLE_Authority)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, TEXT("Server"));
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Green, TEXT("Client"));
+	}
+}
+
+// Called every frame
+void AMyBox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AMyBox::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AMyBox, ReplicatedVar);
+}
+
