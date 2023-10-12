1bf783a70d527fe50ad453fa13a210c18bb3c1ef
Cael Rowley <drdeus@pm.me>
2023-10-12 12:14:10 +0200

    feat: add animation and sfx to key

---
+++ CoopPlatformer/Source/CoopPlatformer/Private/CollectableKey.cpp
@@ -0,0 +1,88 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Net/UnrealNetwork.h"
+#include "../CoopPlatformerCharacter.h"
+
+#include "CollectableKey.h"
+
+// Sets default values
+ACollectableKey::ACollectableKey()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bReplicates = true;
+	SetReplicateMovement(true);
+
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	SetRootComponent(RootComp);
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	Capsule->SetupAttachment(RootComp);
+	Capsule->SetIsReplicated(true);
+	Capsule->SetCollisionProfileName(FName("OverlapAllDynamic"));
+	Capsule->SetCapsuleHalfHeight(150.0f);
+	Capsule->SetCapsuleRadius(100.0f);
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComp);
+	Mesh->SetIsReplicated(true);
+	Mesh->SetCollisionProfileName(FName("OverlapAllDynamic"));
+
+	CollectAudio = CreateDefaultSubobject<UAudioComponent>(TEXT("CollectAudio"));
+	CollectAudio->SetupAttachment(RootComp);
+	CollectAudio->SetAutoActivate(false);
+
+	RotationSpeed = 100.0f;
+}
+
+void ACollectableKey::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ACollectableKey, IsCollected);
+}
+
+// Called when the game starts or when spawned
+void ACollectableKey::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ACollectableKey::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority())
+	{
+		Mesh->AddRelativeRotation(FRotator(0.0f, RotationSpeed * DeltaTime, 0.0f));
+
+		TArray<AActor*> OverlapActors;
+		Capsule->GetOverlappingActors(OverlapActors, ACoopPlatformerCharacter::StaticClass());
+
+		if (!OverlapActors.IsEmpty())
+		{
+			if (!IsCollected)
+			{
+				IsCollected = true;
+				OnRep_IsCollected();
+			}
+		}
+	}
+}
+
+void ACollectableKey::OnRep_IsCollected()
+{
+	if (HasAuthority())
+	{
+		UE_LOG(LogTemp, Display, TEXT("OnRep_IsCollected called from the Server!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Display, TEXT("OnRep_IsCollected called from the Client!"));
+	}
+
+	Mesh->SetVisibility(!IsCollected);
+	CollectAudio->Play();
+}
+
---
+++ CoopPlatformer/Source/CoopPlatformer/Public/CollectableKey.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Components/StaticMeshComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/AudioComponent.h"
+
+#include "CollectableKey.generated.h"
+
+UCLASS()
+class COOPPLATFORMER_API ACollectableKey : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ACollectableKey();
+
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float RotationSpeed;
+
+	UPROPERTY(ReplicatedUsing = OnRep_IsCollected, BlueprintReadWrite, VisibleAnywhere)
+	bool IsCollected;
+
+	UFUNCTION()
+	void OnRep_IsCollected();
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	USceneComponent* RootComp;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UCapsuleComponent* Capsule;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UAudioComponent* CollectAudio;
+
+};
